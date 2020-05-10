858490712a9d2cf5156387b5f337aa72d165be68
Rui Zhou <rui.zhou@sap.com>
2020-05-10 20:44:03 +0800

    commit

---
+++ src/main/java/exe002/HelloPersonDemo.java
@@ -0,0 +1,14 @@
+package exe002;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class HelloPersonDemo {
+
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+        Person person = (Person) context.getBean("helloPerson");      
+        person.SayHello();
+    }
+
+}
